174e162545b4f02e87a692885e51ac44982fd673
Ali Ahmadian <aliash_parthia@hotmail.com>
2020-10-11 16:44:00 -0300

    Initial commit

---
+++ MercadoAli/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MercadoAli
+//
+//  Created by Ali Ahmadian shalchi on 11/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
